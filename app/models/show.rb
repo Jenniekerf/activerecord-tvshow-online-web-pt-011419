a04724eb8e6472e06a5edaf8291576bfe27c7f7f
Jennie Kerfstedt <jenniekerf@gmail.com>
2019-04-03 16:52:59 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -8,7 +8,7 @@ class Show < ActiveRecord::Base
  
  def self.most_popular_show
    #binding.pry
-   Show.where("rating = ?", self.highest_rating)
+   Show.where("rating = ?", self.highest_rating).first
  end 
  
  def self.lowest_rating
